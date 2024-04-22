fc5c45c15b5821b8e5dba016eda1060f13dcd9cd
jezhumble <jez@jezhumble.net>
2024-04-22 13:58:04 -0700

    Refactored to introduce strategy pattern

--- src/edu/berkeley/aep/Airport.java
+++ src/edu/berkeley/aep/Airport.java
@@ -4,55 +4,43 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
 
+// Understands how to navigate through the airport route network
 public class Airport {
-    private final List<Airport> children = new ArrayList<>();
     private final List<Route> routes = new ArrayList<>();
     public final static int UNREACHABLE = -1;
 
     public Airport() {
     }
 
-    public boolean canReach(Airport destination) {
-        return hopsTo(destination, new HashSet<>()) != UNREACHABLE;
-    }
-
     public void addRoute(Airport destination, int cost) {
-        children.add(destination);
         routes.add(new Route(destination, cost));
     }
 
-    public int hopsTo(Airport destination) {
-        return hopsTo(destination, new HashSet<>());
+    public boolean canReach(Airport destination) {
+        return costTo(destination, new HashSet<>(), Route.HOP_STRATEGY) != UNREACHABLE;
     }
 
-    public int hopsTo(Airport destination, Set<Airport> visited) {
-        if (!visited.add(this)) return UNREACHABLE;
-        if (this == destination) return 0;
-        int minHops = UNREACHABLE;
-        for (var child : children) {
-            int hops = child.hopsTo(destination, new HashSet<>(visited));
-            if (hops != UNREACHABLE && (hops < minHops || minHops == UNREACHABLE)) {
-                minHops = hops;
-            }
-        }
-        return minHops == UNREACHABLE ? UNREACHABLE : minHops + 1;
+    public int hopsTo(Airport destination) {
+        return costTo(destination, new HashSet<>(), Route.HOP_STRATEGY);
     }
 
     public int costTo(Airport destination) {
-        return costTo(destination, new HashSet<>());
+        return costTo(destination, new HashSet<>(), Route.COST_STRATEGY);
     }
 
-    public int costTo(Airport destination, Set<Airport> visited) {
+    public int costTo(Airport destination, Set<Airport> visited, Function<Route, Integer> strategy) {
         if (!visited.add(this)) return UNREACHABLE;
         if (this == destination) return 0;
-        int minHops = UNREACHABLE;
+        int minCost = UNREACHABLE;
         for (var route : routes) {
-            int hops = route.costTo(destination, new HashSet<>(visited));
-            if (hops != UNREACHABLE && (hops < minHops || minHops == UNREACHABLE)) {
-                minHops = hops;
+            int cost = route.costTo(destination, new HashSet<>(visited), strategy);
+            if (cost != UNREACHABLE && (cost < minCost || minCost == UNREACHABLE)) {
+                minCost = cost;
             }
         }
-        return minHops;
+        return minCost;
     }
+
 }
--- src/edu/berkeley/aep/Route.java
+++ src/edu/berkeley/aep/Route.java
@@ -1,19 +1,25 @@
 package edu.berkeley.aep;
 
 import java.util.HashSet;
+import java.util.function.Function;
 
+// Understands how to compute the cost between two airports
 public class Route {
-    private final Airport route;
+    private final Airport next;
     private final int cost;
 
-    public Route(Airport route, int cost) {
-        this.route = route;
+    public static Function<Route, Integer> HOP_STRATEGY = route -> 1;
+
+    public static Function<Route, Integer> COST_STRATEGY = route -> route.cost;
+
+    public Route(Airport next, int cost) {
+        this.next = next;
         this.cost = cost;
     }
 
-    public int costTo(Airport destination, HashSet<Airport> visited) {
-        var path = route.costTo(destination, visited);
-        if (path == Airport.UNREACHABLE) return Airport.UNREACHABLE;
-        return path + cost;
+    public int costTo(Airport destination, HashSet<Airport> airports, Function<Route, Integer> strategy) {
+        int costToNext = next.costTo(destination, airports, strategy);
+        if (costToNext == Airport.UNREACHABLE) { return Airport.UNREACHABLE; }
+        return costToNext + strategy.apply(this);
     }
 }
